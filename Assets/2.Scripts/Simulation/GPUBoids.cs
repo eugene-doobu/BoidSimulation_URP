96186e8afcd646ce2f2f95d84b3acbb590c09a6d
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2021-02-10 15:00:46 +0900

    Avoid Obstacle
    장애물 피하기 훈련

--- Assets/2.Scripts/Simulation/GPUBoids.cs
+++ Assets/2.Scripts/Simulation/GPUBoids.cs
@@ -19,7 +19,7 @@ namespace BoidsSimulationOnGPU
 
         #region Boids Parameters
         // 최대 개체 수
-        [Range(256, 32768)]
+        [Range(256, 65536)]
         public int MaxObjectNum = 16384;
 
         // 결합을 적용하는 다른 개체와의 반경
@@ -43,6 +43,10 @@ namespace BoidsSimulationOnGPU
 
         // 벽을 피하는 힘의 시뮬레이션 가중치
         public float AvoidWallWeight = 10.0f;
+        // 장애물을 피하는 힘의 시뮬레이션 가중치
+        public float AvoidObstacleWeight = 100.0f;
+        // 장애물을 감지하는 거리(시야)
+        public float AvoidObstacleDistance = 0.9f;
 
         // 벽의 중심 좌표
         public Vector3 WallCenter = Vector3.zero;
@@ -60,6 +64,8 @@ namespace BoidsSimulationOnGPU
         ComputeBuffer _boidForceBuffer;
         // Boid의 기본 데이터(속도, 위치)를 포함하는 버퍼
         ComputeBuffer _boidDataBuffer;
+        // Obstacle의 위치를 포함하는 버퍼
+        ComputeBuffer _obstacleDataBuffer;
         // 현재 Boid 데이터를 저장
         BoidData[] currBoidDataArray;
         #endregion
@@ -129,20 +135,26 @@ namespace BoidsSimulationOnGPU
                 Marshal.SizeOf(typeof(BoidData)));
             _boidForceBuffer = new ComputeBuffer(MaxObjectNum,
                 Marshal.SizeOf(typeof(Vector3)));
-
+            _obstacleDataBuffer = new ComputeBuffer(MaxObjectNum,
+                Marshal.SizeOf(typeof(Vector3)));
+            
             // Boid 데이터, fORCE 버퍼를 초기화
             var forceArr = new Vector3[MaxObjectNum];
             var boidDataArr = new BoidData[MaxObjectNum];
+            var obstacleArr = new Vector3[MaxObjectNum];
             for (var i = 0; i < MaxObjectNum; i++)
             {
                 forceArr[i] = Vector3.zero;
                 boidDataArr[i].Position = Random.insideUnitSphere * 1.0f;
                 boidDataArr[i].Velocity = Random.insideUnitSphere * 0.1f;
+                obstacleArr[i] = Vector3.zero;
             }
             _boidForceBuffer.SetData(forceArr);
             _boidDataBuffer.SetData(boidDataArr);
+            _obstacleDataBuffer.SetData(obstacleArr);
             forceArr = null;
             boidDataArr = null;
+            obstacleArr = null;
         }
 
         // 시뮬레이션
@@ -168,22 +180,54 @@ namespace BoidsSimulationOnGPU
             cs.SetVector("_WallCenter", WallCenter);
             cs.SetVector("_WallSize", WallSize);
             cs.SetFloat("_AvoidWallWeight", AvoidWallWeight);
+            cs.SetFloat("_AvoidObstacleWeight", AvoidObstacleWeight);
             cs.SetBuffer(id, "_BoidDataBufferRead", _boidDataBuffer);
             cs.SetBuffer(id, "_BoidForceBufferWrite", _boidForceBuffer);
             cs.Dispatch(id, threadGroupSize, 1, 1); // ComputeShader를 실행
 
+
             // 계산된 조항력으로부터 속도와 위치를 업데이트
             id = cs.FindKernel("IntegrateCS"); // 커널 ID를 가져옴
-            cs.SetFloat("_DeltaTime", Time.deltaTime);
-            cs.SetBuffer(id, "_BoidForceBufferRead", _boidForceBuffer);
-            cs.SetBuffer(id, "_BoidDataBufferWrite", _boidDataBuffer);
-            cs.Dispatch(id, threadGroupSize, 1, 1); // ComputeShader를 실행
 
             // 계산된 데이터 저장
             this._boidDataBuffer.GetData(currBoidDataArray);
-            Debug.Log("@@@@@@@@@@@@@");
-            Debug.Log(currBoidDataArray[0].Position);
-            Debug.Log(currBoidDataArray[0].Velocity);
+
+            // 장애물 회피 계산
+            var obstacleArr = new Vector3[MaxObjectNum];
+            for(int i=0; i< MaxObjectNum; i++)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(currBoidDataArray[i].Position,
+                    currBoidDataArray[i].Velocity,
+                    out hit,
+                    AvoidObstacleDistance))
+                {
+                    /*
+                    Debug.DrawRay(currBoidDataArray[i].Position,
+                        currBoidDataArray[i].Velocity.normalized * AvoidObstacleDistance,
+                        Color.red);
+                    */
+                    obstacleArr[i] = Vector3.Normalize(currBoidDataArray[i].Position - hit.point);
+                }
+                else
+                {
+                    /*
+                    Debug.DrawRay(currBoidDataArray[i].Position,
+                        currBoidDataArray[i].Velocity.normalized * AvoidObstacleDistance,
+                        Color.green);
+                    */
+                    obstacleArr[i] = Vector3.zero;
+                }
+            }
+
+            // 컴퓨트 셰이더에 데이터 입력
+            _obstacleDataBuffer.SetData(obstacleArr);
+            cs.SetFloat("_DeltaTime", Time.deltaTime);
+            cs.SetBuffer(id, "_ObstaclePosBufferRead", _obstacleDataBuffer);
+            cs.SetBuffer(id, "_BoidForceBufferRead", _boidForceBuffer);
+            cs.SetBuffer(id, "_BoidDataBufferWrite", _boidDataBuffer);
+            cs.Dispatch(id, threadGroupSize, 1, 1); // ComputeShader를 실행
+            obstacleArr = null;
         }
 
         // 버퍼를 해제
@@ -199,6 +243,11 @@ namespace BoidsSimulationOnGPU
             {
                 _boidForceBuffer.Release();
                 _boidForceBuffer = null;
+            }
+            if (_obstacleDataBuffer != null)
+            {
+                _obstacleDataBuffer.Release();
+                _obstacleDataBuffer = null;
             }
         }
         #endregion
