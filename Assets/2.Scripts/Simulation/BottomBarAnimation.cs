cde65a6bf31d2c75d672d1e366a311c4ca10e214
eugene-doobu <ssundong0_0@naver.com>
2021-06-02 14:17:06 +0900

    change plugins to UPM
    for sonarcloud

--- Assets/2.Scripts/Simulation/BottomBarAnimation.cs
+++ Assets/2.Scripts/Simulation/BottomBarAnimation.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using UniRx;
+using UniRx;
+using DG.Tweening;
 using System;
 
 public class BottomBarAnimation : MonoBehaviour
@@ -14,7 +15,7 @@ public class BottomBarAnimation : MonoBehaviour
     Transform group_panelsTr;
     Text[] panels;
 
-    //ReactiveProperty<float> selectedPos = new ReactiveProperty<float>(0);
+    ReactiveProperty<float> selectedPos = new ReactiveProperty<float>(0);
     Transform selectedTr;
     #endregion
 
@@ -31,7 +32,6 @@ public class BottomBarAnimation : MonoBehaviour
 
     void Start()
     {
-        /*
         // 씬이 로딩되고 일정 시간동안 UI의 Transfrom값이 불안정함
         // 원인 파악이 안되어 일단은 상수값 입력 ㅡ       
         // 상수값은 화면 전체 가로 넓이에서 첫번째 패널의 중앙값의 위치 비율    
@@ -43,11 +43,10 @@ public class BottomBarAnimation : MonoBehaviour
             .Subscribe(_ =>
             {
                 BtnsEnable(false);
-                //selectedTr.DOMoveX(selectedPos.Value, 1f)
-                //    .OnComplete(() => BtnsEnable(true));
+                selectedTr.DOMoveX(selectedPos.Value, 1f)
+                    .OnComplete(() => BtnsEnable(true));
                 // 플레이어 이동 애니메이션 이벤트
             });
-        */
     }
     #endregion
 
